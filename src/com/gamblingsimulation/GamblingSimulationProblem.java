06bc3de1bbd4d2285b4f6d4c741a6432eab3aa87
Priya <Priyamadiwalar0007@gmail.com>
2022-02-09 12:14:18 +0530

    added:GamblerStartWithStake

--- src/com/gamblingsimulation/GamblingSimulationProblem.java
+++ src/com/gamblingsimulation/GamblingSimulationProblem.java
@@ -4,7 +4,15 @@ public class GamblingSimulationProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Welcom to the GamblerSimulator");
+         System.out.println("Welcom to the GamblerSimulator");
+       
+         int amount = 100;
+		 int bet = 1;
+		 System.out.println("Amount of stake is $"+amount);
+		 System.out.println("Amount to bet after every game $"+bet);
 	}
 
 }
+	
+
+
