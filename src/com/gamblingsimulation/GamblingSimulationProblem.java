410c4f80c828483056e7a7d281db58422ef2b040
Priya <Priyamadiwalar0007@gmail.com>
2022-02-09 13:12:36 +0530

    added: GamblerWinOrLoose

--- src/com/gamblingsimulation/GamblingSimulationProblem.java
+++ src/com/gamblingsimulation/GamblingSimulationProblem.java
@@ -1,18 +1,38 @@
 package com.gamblingsimulation;
-import java.util.*;
-public class GamblingSimulationProblem {
 
+import java.util.*;
+
+  public class GamblingSimulationProblem {
+	  
+	   static int AMOUNT = 100;
+	   static int BET = 1;
+	   static int WON = 1;
+	   static int LOST =0;
+	   
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Welcom to the GamblerSimulator");
-       
-         int amount = 100;
-		 int bet = 1;
-		 System.out.println("Amount of stake is $"+amount);
-		 System.out.println("Amount to bet after every game $"+bet);
-	}
-
-}
-	
-
+     
+	     int play = (int) Math.floor(Math.random() * 10) % 2;
+	     System.out.println("Gamblers play:"+play);
+	     
+	     int cashInHand = AMOUNT;
+		 switch(play) {
+		 	case 1:
+		 		   cashInHand = cashInHand + BET;
+		 		   System.out.println("Total current cash gambler has "+cashInHand);
+		 		   System.out.println("Player won the first play");
+		 		   break;
+		 	case 2:
+		 		   cashInHand = cashInHand - BET;
+		 		   System.out.println("Total current cash gambler has "+cashInHand);
+		 		   System.out.println("Player lost the first play");
+		 		   break;
+		   default:
+		 		   System.out.println("default");
+		 }
+	 }
+  }
+	 
+    	 
 
