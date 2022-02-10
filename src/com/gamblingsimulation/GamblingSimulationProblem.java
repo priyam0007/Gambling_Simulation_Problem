fed0dd0c2930edf00012189825062f4a64809c91
Priya <Priyamadiwalar0007@gmail.com>
2022-02-10 22:34:25 +0530

    Added: WonOrLost50%

--- src/com/gamblingsimulation/GamblingSimulationProblem.java
+++ src/com/gamblingsimulation/GamblingSimulationProblem.java
@@ -1,38 +1,51 @@
 package com.gamblingsimulation;
 
-import java.util.*;
+ import java.util.*;
 
-  public class GamblingSimulationProblem {
+   public class GamblingSimulationProblem {
+	  static int AMOUNT_OF_STAKE=100;
+		static int BET_AMOUNT=1;
+		static int WON=1;
+		static int LOST=0;
+		public static void main(String[] args) {
+			 Scanner sc = new Scanner(System.in);
+			 System.out.println("Starting amount of stake is "+AMOUNT_OF_STAKE);
+			 System.out.println("Amount to bet after every play "+BET_AMOUNT);
+			 int cashInHand = AMOUNT_OF_STAKE;
+			 while(cashInHand > 50 && cashInHand < 150) {
+				 int play = (int) Math.floor(Math.random() * 10) % 2;
+				 System.out.println("Gamblers play:"+play);
+			 
+				 switch(play) {
+			 		case 1:
+			 			cashInHand = cashInHand + BET_AMOUNT;
+			 			System.out.println("Total current cash gambler has "+cashInHand);
+			 			break;
+			 		case 0:
+			 			cashInHand = cashInHand - BET_AMOUNT;
+			 			System.out.println("Total current cash gambler has "+cashInHand);
+			 			break;
+			 		default:
+			 			System.out.println("default");
+				 }
+				 if(play == 1) {
+					 System.out.println("Player won the current play");
+				 }
+				 else {
+					 System.out.println("Player lost the current play");
+				 }
+			 
+			 }
+			 if(cashInHand==150 || cashInHand==50) {
+				 System.out.println("--------------------------------------------");
+				 System.out.println("Player would like to resign for current day");
+				 System.out.println("--------------------------------------------");
+			 }
+		}
+
+	}
+  
 	  
-	   static int AMOUNT = 100;
-	   static int BET = 1;
-	   static int WON = 1;
-	   static int LOST =0;
-	   
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-         System.out.println("Welcom to the GamblerSimulator");
-     
-	     int play = (int) Math.floor(Math.random() * 10) % 2;
-	     System.out.println("Gamblers play:"+play);
-	     
-	     int cashInHand = AMOUNT;
-		 switch(play) {
-		 	case 1:
-		 		   cashInHand = cashInHand + BET;
-		 		   System.out.println("Total current cash gambler has "+cashInHand);
-		 		   System.out.println("Player won the first play");
-		 		   break;
-		 	case 2:
-		 		   cashInHand = cashInHand - BET;
-		 		   System.out.println("Total current cash gambler has "+cashInHand);
-		 		   System.out.println("Player lost the first play");
-		 		   break;
-		   default:
-		 		   System.out.println("default");
-		 }
-	 }
-  }
-	 
+
     	 
 
